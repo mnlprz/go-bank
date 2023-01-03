b0f62eaa7ba8e3bae21f41a40beba434d243f90d
mnlprz <emanuelperez19912@gmail.com>
2023-01-03 14:54:31 -0300

    1

--- storage.go
+++ storage.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"log"
+	"database/sql"
+	"fmt"
 
 	_ "github.com/lib/pq"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 type Storage interface {
@@ -17,16 +16,21 @@ type Storage interface {
 }
 
 type PostgresStore struct {
-	db *gorm.DB
+	db *sql.DB
 }
 
 func NewPostgresStore() (*PostgresStore, error) {
 
 	dsn := "host=localhost user=root password=secret dbname=go-bank port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn))
+	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, err
 	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
 	return &PostgresStore{
 		db: db,
 	}, nil
@@ -34,7 +38,44 @@ func NewPostgresStore() (*PostgresStore, error) {
 
 func (s *PostgresStore) Init() error {
 
-	err := s.db.AutoMigrate(&Account{})
+	err := s.createAccountTable()
+
+	return err
+}
+
+func (s *PostgresStore) createAccountTable() error {
+
+	q := `CREATE TABLE IF NOT EXISTS ACCOUNT(
+			ID SERIAL PRIMARY KEY,
+			FIRST_NAME VARCHAR(50),
+			LAST_NAME VARCHAR(50),
+			NUMBER SERIAL,
+			BALANCE SERIAL,
+			CREATED_AT TIMESTAMP
+		)`
+
+	_, err := s.db.Exec(q)
+
+	return err
+}
+
+func (s *PostgresStore) CreateAccount(acc *Account) error {
+
+	q := `INSERT INTO ACCOUNT(
+			FIRST_NAME,
+			LAST_NAME,
+			NUMBER,
+			BALANCE,
+			CREATED_AT
+			) VALUES
+			($1, $2, $3, $4, $5)`
+
+	_, err := s.db.Exec(q,
+		acc.FirstName,
+		acc.LastName,
+		acc.Number,
+		acc.Balance,
+		acc.CreatedAt)
 	if err != nil {
 		return err
 	}
@@ -42,24 +83,18 @@ func (s *PostgresStore) Init() error {
 	return nil
 }
 
-func (s *PostgresStore) CreateAccount(acc *Account) error {
-
-	id := s.db.Create(&Account{
-		FirstName: acc.FirstName,
-		LastName:  acc.LastName,
-		Number:    acc.Number,
-		Balance:   acc.Balance,
-		CreatedAt: acc.CreatedAt,
-	})
-
-	log.Println("Create accoun: ", id)
-
-	return nil
-}
-
 func (s *PostgresStore) DeleteAccountByID(id int) error {
 
-	s.db.Delete(&Account{}, id)
+	q := `DELETE FROM ACCOUNT WHERE ID = $1`
+	r, err := s.db.Exec(q, id)
+	if err != nil {
+		return err
+	}
+
+	if rf, _ := r.RowsAffected(); rf == 0 {
+		return fmt.Errorf("id: %d not found to delete", id)
+	}
+
 	return nil
 }
 
@@ -70,18 +105,36 @@ func (s *PostgresStore) UpdateAccount(*Account) error {
 
 func (s *PostgresStore) GetAccountByID(id int) (*Account, error) {
 
+	q := `SELECT * FROM ACCOUNT WHERE ID = $1`
+
 	account := new(Account)
 
-	s.db.First(&account, id)
+	err := s.db.QueryRow(q, id).Scan(
+		&account.ID,
+		&account.FirstName,
+		&account.LastName,
+		&account.Number,
+		&account.Balance,
+		&account.CreatedAt,
+	)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("id: %d not found", id)
+		}
+		return nil, err
+	}
 
 	return account, nil
 }
 
 func (s *PostgresStore) GetAccounts() ([]*Account, error) {
 
-	/* var accounts []*Account
-	result := s.db.Find(&accounts)
-	result.Scan()
+	rows, err := s.db.Query(`SELECT * FROM ACCOUNT`)
+	if err != nil {
+		return nil, err
+	}
+
+	accounts := []*Account{}
 
 	for rows.Next() {
 		account := new(Account)
@@ -99,6 +152,5 @@ func (s *PostgresStore) GetAccounts() ([]*Account, error) {
 		accounts = append(accounts, account)
 	}
 
-	return accounts, nil */
-	return nil, nil
+	return accounts, nil
 }
